7c665a6f512c72d0de16978f4dd189cab92a9c4d
tang <eytang8@gmail.com>
2020-05-22 21:44:58 -0700

    now handles application logic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
-const port = 4000;
+const port = 3000;
 const bodyParser = require('body-parser');
 const { Client } = require('pg');
 const client = new Client();
@@ -11,7 +11,7 @@ client.connect();
 let pass;
 console.log('retreiving pass from db');
 client.query('select * from users where id = 1', (err, ans)=>{
-  if(ans.rows[0] && ans.rows[0].password){
+  if(ans && ans.rows[0] && ans.rows[0].password){
     pass = ans.rows[0].password;
     console.log('cached pass on server');
   }
@@ -21,33 +21,40 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res)=>{
-  res.send('yooooooo welcome to my twisted world');
+app.use(express.static('../home/build'));
+//app.get('/', express.static('../home/build'));
+
+app.use((err, req, res, next)=>{
+  console.error(err.stack);
+  res.status(500).send(err);
 });
 
-app.post('/api/posts', (req, res)=>{
-  const post = req.body;
-  console.log('receiving post submission', post);
-  if(post.pass === pass){
-    console.log('authenticated!');
-    client.query(
-      'insert into posts(author, body) values ($1, $2)', 
-      [post.author, post.body], 
-      (err, ans)=>{
-        if(err){
-          console.log('err', err)
-          return res.send('error while posting!');
-        }
-        console.log(ans);
-        return res.send('confirmed!')
-      })
+const authController = (req, res, next)=>{
+  console.log('receiving password', req.body.pass);
+  if(req.body.pass !== pass){
+    return next("auth error!");
   }else{
-    console.log('nope');
-    return res.send('nope');
+    console.log('auth success');
+    return next();
   }
+}
+
+app.post('/api/posts', authController, (req, res)=>{
+  const post = req.body;
+  client.query(
+    'insert into posts(author, body) values ($1, $2)', 
+    [post.author, post.body], 
+    (err, ans)=>{
+      if(err){
+        return next('error while posting!');
+      }
+      console.log(ans);
+      return res.send('confirmed!')
+    })
 });
 
 app.get('/api/posts/:id', (req, res)=>{
+  console.log('retrieving');
   if(req.params.id === '0'){
     console.log('getting most recent post');
     client.query('select * from posts order by id desc limit 1',(err, ans)=>{
